4b99baf29b1ef397f3b34d83d0531ac30230e105
Constantin Leyendecker <constantinleyend@gmail.com>
2023-05-24 16:00:15 +0800

    Challenge 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(name: 'Pizza place', address: 'Canggu', phone_number: '0123456789', category: 'chinese')
+Restaurant.create(name: 'Subway', address: 'Berlin', phone_number: '015496789', category: 'italian')
+Restaurant.create(name: 'McDonalds', address: 'Paris', phone_number: '0584296789', category: 'japanese')
+Restaurant.create(name: 'Burger King', address: 'London', phone_number: '058256789', category: 'french')
+Restaurant.create(name: 'Lolas', address: 'New York', phone_number: '0122849789', category: 'belgian')
